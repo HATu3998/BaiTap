e300885d36ee5f02f6ac9855b9441242823b64f5
AnhTu <3artemis98@gmail.com>
2023-07-20 23:53:16 +0700

    DeQuyGiaiThua

---
+++ src/main/java/BaiTap/DeQuyGiaiThua.java
@@ -0,0 +1,23 @@
+package BaiTap;
+import java.util.Scanner;
+public class DeQuyGiaiThua {
+
+	  public static void main(String[] args){
+		    Scanner input=new Scanner(System.in);int n=input.nextInt();
+		    
+		      if (n <= 20) {
+		            long factorial = calculateFactorial(n);
+		            System.out.println(n + "! = " + factorial);
+		        } else {
+		            System.out.println("n phai nho hon hoac bang 20.");
+		        }
+		  }
+		  
+		    public static long calculateFactorial(int n) {
+		        long result = 1;
+		        for (int i = 1; i <= n; i++) {
+		            result *= i;
+		        }
+		        return result;
+		    }
+}
