05eda2829d87db914671f9ed48cac1e8eb6c613a
AnhTu <3artemis98@gmail.com>
2023-08-04 02:06:24 +0700

    xóa phần tử giống nhiều hơn 2

--- src/main/java/BaiTap/XoaPhanTuGiongNhau.java
+++ src/main/java/BaiTap/XoaPhanTuGiongNhau.java
@@ -11,4 +11,16 @@ public class XoaPhanTuGiongNhau {
 	       }
 	       return k;
 	    }
+	
+	//phần tử xuất hiện nhiều hơn 2
+    public int removeDuplicatesNum2(int[] nums) {
+        int k=2;
+         for(int i=2;i<nums.length;i++){
+             if(nums[k-2]!=nums[i]){
+                 nums[k]=nums[i];
+                 k++;
+             }
+         }
+         return k;
+    }
 }
