5dddae423226e65f0aa7bd456bfe44dd820fab3f
AnhTu <3artemis98@gmail.com>
2023-08-07 23:59:11 +0700

    update last length

--- src/main/java/BaiTap/LastLength.java
+++ src/main/java/BaiTap/LastLength.java
@@ -2,14 +2,16 @@ package BaiTap;
 
 public class LastLength {
 	public int lengthOfLastWord(String s) {
-        int count=0;
-        for(int i=s.length()-1;i>0;i--){
-            if(s.charAt(i)!=' '){
-count++;
-            }else{
-                return count;
-            }
-        }
-        return count;
+		  int count=0; int i = s.length() - 1;
+	        while (i >= 0 && s.charAt(i) == ' ') {
+	            i--;
+	        }
+	        
+	        // Count the length of the last word
+	        while (i >= 0 && s.charAt(i) != ' ') {
+	            count++;
+	            i--;
+	        }
+	        return count;
     }
 }
