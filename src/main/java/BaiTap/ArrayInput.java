4bdc41e2a3df5faf3a87588e0cabef3d62204066
AnhTu <3artemis98@gmail.com>
2023-08-20 01:23:34 +0700

    input array target

---
+++ src/main/java/BaiTap/ArrayInput.java
@@ -0,0 +1,24 @@
+package BaiTap;
+
+public class ArrayInput {
+//	Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+//
+//			Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+//
+//			The tests are generated such that there is exactly one solution. You may not use the same element twice.
+//
+//			Your solution must use only constant extra space.
+	
+    public int[] twoSum(int[] numbers, int target) {
+        int[] a=new int[2];
+        int n=0;
+        int left=0;
+        int right=numbers.length-1;
+        while(numbers[left]+numbers[right]!=target){
+          if(numbers[left]+numbers[right]<target)left++;
+          else{right--;}
+        }
+        a[0]=left+1;a[1]=right+1;
+        return a;
+            }
+}
